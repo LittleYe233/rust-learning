1d4d88887dc28ecba10b8d4ee063e5b97c5db2de
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2022-08-21 20:57:55 +0800

    chore(file_struct): update at 2022-08-21T20:57:55Z+08:00

--- file_struct/src/filestruct.rs
+++ file_struct/src/filestruct.rs
@@ -1,5 +1,38 @@
+#![allow(unused_variables)]
+
 #[derive(Debug)]
 pub struct File {
     pub name: String,
-    pub data: Vec<u8>,
+    pub data: Vec<u8>,   
+}
+
+impl File {
+    pub fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
+        }
+    }
+
+    pub fn new_with_data(name: &str, data: &Vec<u8>) -> File {
+        let mut f = File::new(name);
+        f.data = data.clone();
+        f
+    }
+
+    pub fn read(self: &File, save_to: &mut Vec<u8>) -> usize {
+        let mut tmp = self.data.clone();
+        let read_length = tmp.len();
+        save_to.reserve(read_length);
+        save_to.append(&mut tmp);
+        read_length
+    }
+}
+
+pub fn open(f: &mut File) -> bool {
+    true
+}
+
+pub fn close(f: &mut File) -> bool {
+    true
 }
--- file_struct/src/main.rs
+++ file_struct/src/main.rs
@@ -1,14 +1,59 @@
 mod filestruct;
 
+static mut ERROR: i32 = 0;
+
 fn main() {
+    // f1
     let f1 = filestruct::File {
         name: String::from("f1.txt"),
         data: Vec::new(),
     };
 
     let f1_name = &f1.name;
-    let f1_length = &f1.data.len();
+    let f1_length = f1.data.len();
 
     println!("{:?}", f1);
     println!("{} is {} byte(s) long.", f1_name, f1_length);
+
+    // f2
+    let f2 = filestruct::File::new("f2.txt");
+
+    let f2_name = &f2.name;
+    let f2_length = f2.data.len();
+
+    println!("{:?}", f2);
+    println!("{} is {} byte(s) long.", f2_name, f2_length);
+
+    // f3
+    let f3_data: Vec<u8> = vec![114, 117, 115, 116, 33];
+    let mut f3 = filestruct::File::new_with_data("f3.txt", &f3_data);
+    let mut buffer: Vec<u8> = vec![];
+
+    filestruct::open(&mut f3);
+    let f3_length = f3.read(&mut buffer);
+    filestruct::close(&mut f3);
+    let f3_name = &f3.name;
+
+    let text = String::from_utf8_lossy(&buffer);
+
+    println!("{:?}", f3);
+    println!("{} is {} byte(s) long.", f3_name, f3_length);
+    println!("{}", text);
+
+    // f4
+    let mut f4 = filestruct::File::new("f4.txt");
+
+    f4.read(&mut buffer);
+    unsafe {
+        if ERROR != 0 {
+            panic!("An error has occurred while reading the file");
+        }
+    }
+
+    filestruct::close(&mut f4);
+    unsafe {
+        if ERROR != 0 {
+            panic!("An error has occurred while closing the file");
+        }
+    }
 }
