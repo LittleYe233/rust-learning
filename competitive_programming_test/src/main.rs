8cb4f7e039bba120a1d0f64788629fa77c657106
Zhifan Ye <littleye233@gmail.com>
2024-03-28 18:36:24 +0800

    chore(competitive_programming_test): update at 2024-03-28T18:36:24Z+08:00

---
+++ competitive_programming_test/src/main.rs
@@ -0,0 +1,31 @@
+#[allow(unused_imports)]
+use std::io::{BufWriter, stdin, stdout, Write};
+ 
+#[derive(Default)]
+struct Scanner {
+    buffer: Vec<String>
+}
+impl Scanner {
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buffer.pop() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            let mut input = String::new();
+            stdin().read_line(&mut input).expect("Failed read");
+            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+        }
+    }
+}
+
+fn main() {
+    let mut scan = Scanner::default();
+    let out = &mut BufWriter::new(stdout());
+
+    let t = scan.next::<usize>();
+    for _ in 0..t {
+        let a = scan.next::<i64>();
+        let b = scan.next::<i64>();
+        writeln!(out, "{}", a + b).ok();
+    }
+}
